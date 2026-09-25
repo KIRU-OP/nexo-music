9df90e210e01d9df4f6add607c81ad0b8ed1483c
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <np564605@gmail.com>
2026-09-25 07:21:13 +0530

    Uploaded: nexo/__init__.py

---
+++ nexo/__init__.py
@@ -0,0 +1,36 @@
+from nexo.core.bot import JARVIS
+from nexo.core.dir import dirr
+from nexo.core.git import git
+from nexo.core.runtime_patches import apply_runtime_patches
+from nexo.core.userbot import Userbot
+from nexo.misc import dbb, heroku
+from nexo.security import drop_sensitive_env_vars
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+apply_runtime_patches()
+
+app = JARVIS()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
+
+_removed_sensitive_env = drop_sensitive_env_vars()
+if _removed_sensitive_env:
+    LOGGER(__name__).info(
+        "Security hardening active: stripped %s sensitive env vars from process environment.",
+        len(_removed_sensitive_env),
+    )
